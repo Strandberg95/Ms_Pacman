ba0a7af9455911cc537daf1d097e364100cfb2f7
Strandberg95 <strandberg.coffe@gmail.com>
2017-11-09 08:56:22 +0100

    Basic classes added

---
+++ src/pacman/AI/AIBuilder.java
@@ -0,0 +1,4 @@
+package pacman.AI;
+
+public class AIBuilder {
+}
---
+++ src/pacman/AI/DataExtraction/DataExtractionReader.java
@@ -0,0 +1,35 @@
+package pacman.AI.DataExtraction;
+
+import pacman.game.util.IO;
+
+public class DataExtractionReader {
+
+    private static String FileName = "data.txt";
+    private static String selectionSign = " *";
+
+    public static String[] readExtractionList(){
+        String data = IO.loadFile(FileName);
+        String[] dataLine = data.split("\n");
+        return extractSpecifiedDataLines(dataLine,selectionSign);
+    }
+
+    private static int countSequences(String[] dataLine, String sign){
+        int rSize = 0;
+        for(int i = 0; i < dataLine.length; i++){
+            if(dataLine[i].contains(sign)){
+                rSize++;
+            }
+        }
+        return rSize;
+    }
+
+    private static String[] extractSpecifiedDataLines(String[] dataLine, String sign){
+        String[] rArr = new String[countSequences(dataLine,sign)];
+        for(int i = 0,j = 0; i != dataLine.length;i++){
+            if(dataLine[i].contains(sign)){
+                rArr[j++] = dataLine[i].replace(" *","");
+            }
+        }
+        return rArr;
+    }
+}
